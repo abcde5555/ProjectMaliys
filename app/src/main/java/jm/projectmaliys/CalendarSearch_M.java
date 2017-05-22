405253b4bb15386cb6aedf62241c1f13e8d4854f
jm <alraos@naver.com>
2017-05-22 17:23:58 +0900

    v1.17.23 정민 달력 추가

--- app/src/main/java/jm/projectmaliys/CalendarSearch_M.java
+++ app/src/main/java/jm/projectmaliys/CalendarSearch_M.java
@@ -1,9 +1,32 @@
 package jm.projectmaliys;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 //달력검색 클래스
 public class CalendarSearch_M extends Fragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_search__m);
 
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendar);
+
+        //리스너 등록
+        calendarView.setOnDateChangeListener(
+                new CalendarView.OnDateChangeListener(){
+
+                    @Override
+                    public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMenth){
+                        Toast.makeText(
+                                CalendarViewTest.this,
+                                year+"/"+(month+1)+"/"+dayOfMenth,
+                                0
+                                ).show();
+                    }
+                });
+    }
 }
--- app/src/main/java/jm/projectmaliys/MainActivity.java
+++ app/src/main/java/jm/projectmaliys/MainActivity.java
@@ -1,5 +1,6 @@
 package jm.projectmaliys;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LoadingPage_M.class);
+        startActivity(intent);  //로딩페이지 시작
     }
 
     //여긴 메인 왠만하면 건들지 않기~~
