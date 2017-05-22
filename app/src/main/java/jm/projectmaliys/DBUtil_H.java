bcf2f2f99e5214c21a3b849d98ad7eef6f1f7025
charlie-junhyung <june900s@gmail.com>
2017-05-22 17:28:10 +0900

    v1.17.27 준형 DBUtil class 작성중

--- app/src/main/java/jm/projectmaliys/DBUtil_H.java
+++ app/src/main/java/jm/projectmaliys/DBUtil_H.java
@@ -1,5 +1,18 @@
 package jm.projectmaliys;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 // DB 관리
 public class DBUtil_H {
+
+    private static final String dbName = "";
+    private static final String tableName = "";
+    private static final int dbVersion = 1;
+
+    private OpenHelper helper;
+    private SQLiteDatabase db;
+
+    private Context context;
+
 }
