1c49c667679e196c54c55986440443d1c3e5e1c0
charlie-junhyung <june900s@gmail.com>
2017-05-22 17:48:52 +0900

    v1.17.48 준형 DBUtil class 작성중

--- app/src/main/java/jm/projectmaliys/DBUtil_H.java
+++ app/src/main/java/jm/projectmaliys/DBUtil_H.java
@@ -1,7 +1,11 @@
 package jm.projectmaliys;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 // DB 관리
 public class DBUtil_H {
@@ -10,9 +14,77 @@ public class DBUtil_H {
     private static final String tableName = "";
     private static final int dbVersion = 1;
 
-    private OpenHelper helper;
+    private OpenHelper _openHelper;
     private SQLiteDatabase db;
 
-    private Context context;
+    private Context _context;
 
+    public DBUtil_H(Context context) {
+        _context = context;
+        _openHelper = new OpenHelper(context, dbName, null, dbVersion);
+        db = _openHelper.getWritableDatabase();
+    }
+
+    // 다이어리 추가
+    public int insertDiary() {
+
+        return 1;
+    }
+
+    // 다이어리 수정
+    public int updateDiary() {
+
+        return 1;
+    }
+
+    // 다이어리 사진 추가
+    public int updateDiaryPhotos() {
+
+        return 0;
+    }
+
+    // 다이어리 삭제
+    public int deleteDiary() {
+
+        return 0;
+    }
+
+    // 다이어리 목록보기
+    public Cursor selectAllDiary() {
+
+        return null;
+    }
+
+    // 다이어리 자세히 보기
+    public Cursor selectDiary() {
+        return null;
+    }
+
+
+
+    // 데이터베이스를 열거나 업그레이드하는 것을 도와주는 내부 클래스
+    private class OpenHelper extends SQLiteOpenHelper {
+
+        public OpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, null, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String createTableQuery = "";
+            db.execSQL(createTableQuery);
+            Toast.makeText(_context, "DB is opened", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.i("onUpgrade()", "Database is upgraded from "+ oldVersion + " to " + newVersion);
+            Toast.makeText(_context,
+                    "Database is upgraded from " + oldVersion +
+                            " to " + newVersion, Toast.LENGTH_SHORT)
+                    .show();
+
+            onCreate(db);
+        }
+    }
 }
