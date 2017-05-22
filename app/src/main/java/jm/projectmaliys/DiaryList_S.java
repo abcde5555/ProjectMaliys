f038a4b021c5b1f270952884b4d255d2e45cde9f
ParkSeol <psps3020@gmail.com>
2017-05-22 16:35:16 +0900

    v1.16.35 박설

--- app/src/main/java/jm/projectmaliys/DiaryList_S.java
+++ app/src/main/java/jm/projectmaliys/DiaryList_S.java
@@ -40,7 +40,6 @@ public class DiaryList_S extends Fragment {
             super(inflater.inflate(R.layout.activity_diary_list_s, parent, false));
             avator = (ImageView) itemView.findViewById(R.id.list_avatar);
             name = (TextView) itemView.findViewById(R.id.list_title);
-            description = (TextView) itemView.findViewById(R.id.list_desc);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -60,31 +59,13 @@ public class DiaryList_S extends Fragment {
         // Set numbers of List in RecyclerView.
         private static final int LENGTH = 18;
 
-        private final String[] mPlaces;
-        private final String[] mPlaceDesc;
-        private final Drawable[] mPlaceAvators;
-
         public ContentAdapter(Context context) {
             Resources resources = context.getResources();
-            mPlaces = resources.getStringArray(R.array.places);
-            mPlaceDesc = resources.getStringArray(R.array.place_desc);
-            TypedArray a = resources.obtainTypedArray(R.array.place_avator);
-            mPlaceAvators = new Drawable[a.length()];
-            for (int i = 0; i < mPlaceAvators.length; i++) {
-                mPlaceAvators[i] = a.getDrawable(i);
-            }
-            a.recycle();
+
         }
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.avator.setImageDrawable(mPlaceAvators[position % mPlaceAvators.length]);
-            holder.name.setText(mPlaces[position % mPlaces.length]);
-            holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
         }
 
         @Override
